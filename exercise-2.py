5f3802151965e9763b2062583320b56059fde7b8
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-12-10 15:07:42 -0600

    EX2 done

--- exercise-2.py
+++ exercise-2.py
@@ -3,20 +3,12 @@
 # Write the code that:
 # 1. Prompts the user to enter a phrase:
 #      Please enter a word or phrase:
-
 # 2. Print the following message:
 #      - What you entered is xx characters long
-
 # 3. Return to step 1, unless the word 'quit' was entered.
 
 
-
-
-
-# x = None
-# word = len(x)
-
-# for word in len(0):
-#     word = input("Please enter a word or phrase: ").lower()
-# else:
-#     phrase = f"What you entered is {word} characters long."
+word = ""
+while word != "QUIT":
+    word = input("Please enter a word or phrase: ").lower()
+    print (f"What you entered is {len(word)} characters long")
