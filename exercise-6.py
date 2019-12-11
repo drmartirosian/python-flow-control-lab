a2bf924fae107930111abf6304c73a8ba5eca1a7
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-12-11 16:44:17 -0600

    EX 6 done

--- exercise-6.py
+++ exercise-6.py
@@ -3,15 +3,38 @@
 # Write the code that:
 # 1. Prompts the user to enter the month (as three characters):
 #      Enter the month of the season (Jan - Dec):
-# 2. Then propts the user to enter the day of the month: 
+month = input("Enter the month of the season (Jan - Dec): ")
+
+
+# 2. Then propts the user to enter the day of the month:
 #      Enter the day of the month:
+day = int(input("Enter the day of the month 1-31: "))
+
+
 # 3. Calculate what season it is based upon this chart:
 #      Dec 21 - Mar 19: Winter
 #      Mar 20 - Jun 20: Spring
 #      Jun 21 - Sep 21: Summer
 #      Sep 22 - Dec 20: Fall
+
+#WINTER
+if month in ('Dec') and day >= 21 or month in ('Jan', 'Feb') or month in ('Mar') and day <= 19:
+    print('WINTER')
+#SPRING
+if month in ('Mar') and day >= 20 or month in ('Apr', 'May') or month in ('Jun') and day <= 20:
+    print('SPRING')
+#SUMMER
+if month in ('Jun') and day >= 21 or month in ('July', 'Aug') or month in ('Sep') and day <= 21:
+    print('SUMMER')
+#FALL
+if month in ('Sep') and day >= 22 or month in ('Oct', 'Nov') or month in ('Dec') and day <= 20:
+    print('FALL')
+
+
+
 # 4. Print the result as follows:
-#      <Mmm> <dd> is in <season> 
+#      <Mmm> <dd> is in <season>
+#print()
 
 # Hints:
 # Consider using the in operator to check if a string is in a particular list/tuple like this:
